82c3c1f8f61a533d8d889ab03e2db8821cdd50a1
Marc Martinez <universal11@Marcs-MacBook-Pro.local>
2014-04-14 23:42:47 -0700

    initial commit

---
+++ ImageMapper.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package imagemapper;
+
+import gui.ava.html.image.generator.HtmlImageGenerator;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author universal11
+ */
+public class ImageMapper {
+    
+    public static String readFile(String path) throws FileNotFoundException, IOException{
+        File file = new File(path);
+        String content = "";
+        
+        if(file.exists()){
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String contentLine;
+            while( (contentLine = bufferedReader.readLine()) != null){
+               content += contentLine;
+            }
+        }
+        
+        return content;
+    }
+
+    public static void main(String[] args){
+        String inputPath = "";
+        String outputPath = "";
+        String name = "map";
+        
+        for(int i = 0; i < args.length; i++){
+            switch(args[i]){
+                case "-i":
+                    inputPath = args[(i + 1)];
+                    break;
+                case "-o":
+                    outputPath = args[(i + 1)];
+                    break;
+                case "-n":
+                    name = args[(i + 1)];
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        if(!inputPath.isEmpty() && !outputPath.isEmpty()){
+            try {
+                String input = ImageMapper.readFile(inputPath);
+                HtmlImageGenerator imageGenerator = new HtmlImageGenerator();
+                imageGenerator.loadUrl("file://" + inputPath);
+                //imageGenerator.loadHtml(input);
+                imageGenerator.saveAsImage(name + ".png");
+                imageGenerator.saveAsHtmlWithMap(name + ".html", name + ".png");
+            } 
+            catch (IOException ex) {
+                Logger.getLogger(ImageMapper.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+        
+        
+    }
+    
+}
